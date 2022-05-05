5aa369c4c00c43640ee4301e6dc49add5f4e0be9
el a5tabot <youssef20191700793@cis.asu.edu.eg>
2022-05-06 00:57:29 +0200

    Added crystal report project
    Connected and disconnected are not combined with crystal report yet
    
    Co-authored-by: abanoubgamal33 <abanoubgamal33@gmail.com>
    Co-authored-by: Kerolosnabil7 <Kerolosnabil744@gmail.com>
    Co-authored-by: michael70magdy <mikofaxceel@gmail.com>

---
+++ Mile stone 2/CrystalReport/CrystalReport/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrystalReport;
+namespace CrystalReport
+{
+    public partial class Form1 : Form
+    {
+        CrystalReport1 CR;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CR = new CrystalReport1();
+            crystalReportViewer1.ReportSource = CR;
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
